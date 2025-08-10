1f3dfe74720ee9ede5f23ed7628df9eb56897f5a
Sofia Slobodskaya <soniakorolyok@gmail.com>
2025-08-10 19:33:13 +0300

    Added h3 and button + styles

--- src/components/Operation.jsx
+++ src/components/Operation.jsx
@@ -6,17 +6,24 @@ const Operation = () => {
     const [sum, setSum] = useState(0);
     const dispatch = useDispatch();
     return (
-        <div className={'flex justify-center'}>
-            <button className={'bg-blue-600 hover:bg-blue-700 text-white font-bold text-lg rounded-lg py-2 px-4'}
-                onClick={() => dispatch(withdraw(sum))}>Withdraw</button>
-            <input className={'text-center border rounded-lg py-2 px-4 text-white text-lg font-bold'}
-                type="number"
-                   onChange={e => setSum(+e.target.value)}
-                   value={sum}
-            />
-            <button className={'bg-blue-600 hover:bg-blue-700 text-white font-bold text-lg rounded-lg py-2 px-4'}
-                onClick={() => dispatch(deposit(sum))}>Deposit</button>
-        </div>
+        <>
+            <div className={'flex justify-center'}>
+                <button className={'bg-blue-600 hover:bg-blue-700 text-white font-bold text-lg rounded-lg py-2 px-4'}
+                        onClick={() => dispatch(withdraw(sum))}>Withdraw
+                </button>
+                <input className={'text-center border rounded-lg py-2 px-4 text-white text-lg font-bold'}
+                       type="number"
+                       onChange={e => setSum(+e.target.value)}
+                       value={sum}
+                />
+                <button className={'bg-blue-600 hover:bg-blue-700 text-white font-bold text-lg rounded-lg py-2 px-4'}
+                        onClick={() => dispatch(deposit(sum))}>Deposit
+                </button>
+            </div>
+            <div className={'flex justify-center'}>
+                <button className={"bg-blue-400 hover:bg-blue-300 cursor-pointer text-white font-bold text-lg rounded-lg px-9 mt-3 py-2"}>Get Quote</button>
+            </div>
+        </>
     );
 };
 
